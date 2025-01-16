a2d3a542f4ba9d463f8edec14e08a8db2b940ea2
SritharanSri <ssrikalai2255@gmail.com>
2025-01-16 21:24:46 +0530

    Update the DB Config

--- src/controllers/busController.js
+++ src/controllers/busController.js
@@ -42,7 +42,7 @@ export const createBus = async (req, res) => {
 
 export const getBuses = async (req, res) => {
   try {
-    const buses = await Bus.find().populate('route', 'name startLocation endLocation'); // Populating route details
+    const buses = await Bus.find()
     res.status(200).json(buses);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -52,7 +52,7 @@ export const getBuses = async (req, res) => {
 
 export const getBusById = async (req, res) => {
   try {
-    const bus = await Bus.findById(req.params.id).populate('route', 'name startLocation endLocation');
+    const bus = await Bus.findById(req.params.id)
     if (!bus) {
       return res.status(404).json({ message: 'Bus not found' });
     }
